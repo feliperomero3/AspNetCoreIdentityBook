68306554abaf4a6580276fa12914d592b19d7e87
Felipe Romero <feliperomero3@outlook.com>
2021-06-26 23:48:13 -0500

    Create the home page.

--- IdentityApp/Pages/Index.cshtml.cs
+++ IdentityApp/Pages/Index.cshtml.cs
@@ -1,25 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+using IdentityApp.Data;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using SportsStore.Entities;
 
 namespace IdentityApp.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDbContext _context;
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ApplicationDbContext context, ILogger<IndexModel> logger)
         {
+            _context = context;
             _logger = logger;
         }
 
-        public void OnGet()
-        {
+        public IEnumerable<Product> Products { get; set; }
 
+        public PageResult OnGet()
+        {
+            Products = _context.Products.ToArray();
+
+            return Page();
         }
     }
 }
