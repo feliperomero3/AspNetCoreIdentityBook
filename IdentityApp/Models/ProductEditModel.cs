66ea17dc40195ef18ac3e8fe3affa99bc5f9c1bf
Felipe Romero <feliperomero3@outlook.com>
2021-07-05 22:30:26 -0500

    Create the Edit page.

---
+++ IdentityApp/Models/ProductEditModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using IdentityApp.Entities;
+
+namespace IdentityApp.Models
+{
+    public class ProductEditModel
+    {
+        [Required]
+        public long ProductId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        [Required]
+        [Range(typeof(decimal), "1", "9999999999999", ErrorMessage = "Price must be at least 1.")]
+        public decimal Price { get; set; }
+
+        public Product MapToProduct()
+        {
+            return new Product(Name, Description, Category, Price);
+        }
+
+        public static ProductEditModel MapFromProduct(Product product)
+        {
+            if (product == null)
+            {
+                throw new ArgumentNullException(nameof(product));
+            }
+
+            return new ProductEditModel()
+            {
+                ProductId = product.ProductId,
+                Name = product.Name,
+                Description = product.Description,
+                Category = product.Category,
+                Price = product.Price
+            };
+        }
+    }
+}
---
+++ IdentityApp/Pages/Edit.cshtml.cs
@@ -0,0 +1,70 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using IdentityApp.Data;
+using IdentityApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityApp.Pages
+{
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EditModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public ProductEditModel ProductModel { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.FirstOrDefaultAsync(m => m.ProductId == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            ProductModel = ProductEditModel.MapFromProduct(product);
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var product = await _context.Products.FindAsync(ProductModel.ProductId);
+
+            _context.Entry(product).CurrentValues.SetValues(ProductModel);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException) when (!ProductExists(ProductModel.ProductId))
+            {
+                return NotFound();
+            }
+
+            return RedirectToPage("./Admin");
+        }
+
+        private bool ProductExists(long id)
+        {
+            return _context.Products.Any(e => e.ProductId == id);
+        }
+    }
+}
