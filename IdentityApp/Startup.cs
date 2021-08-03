138c6f9751c99c79d3b9eff5d9ea08d6f0a73678
Felipe Romero <feliperomero3@outlook.com>
2021-08-03 00:15:09 -0500

    Configure Facebook Authentication.

--- IdentityApp/Startup.cs
+++ IdentityApp/Startup.cs
@@ -45,6 +45,13 @@ namespace IdentityApp
                     return new SmtpClient(hostname, port);
                 });
             services.AddScoped<IEmailSender, EmailSender>();
+
+            services.AddAuthentication()
+                .AddFacebook(options =>
+                {
+                    options.AppId = Configuration["Facebook:AppId"];
+                    options.AppSecret = Configuration["Facebook:AppSecret"];
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
