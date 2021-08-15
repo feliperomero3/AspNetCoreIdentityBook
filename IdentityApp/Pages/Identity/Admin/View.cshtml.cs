f0090e4f4078dd4abafc04251b451c1dce4f4ab1
Felipe Romero <feliperomero3@outlook.com>
2021-08-14 21:27:17 -0500

    View User Details.

---
+++ IdentityApp/Pages/Identity/Admin/View.cshtml.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityApp.Pages.Identity.Admin
+{
+    public class ViewModel : AdminPageModel
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public ViewModel(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IdentityUser IdentityUser { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string Id { get; set; }
+
+        public IEnumerable<string> PropertyNames => typeof(IdentityUser).GetProperties().Select(p => p.Name);
+
+        public string GetPropertyValue(string name) => typeof(IdentityUser).GetProperty(name).GetValue(IdentityUser)?.ToString();
+
+        public async Task<ActionResult> OnGet()
+        {
+            if (string.IsNullOrEmpty(Id))
+            {
+                return RedirectToPage("SelectUser", new { Label = "View User", Callback = "View" });
+            }
+            IdentityUser = await _userManager.FindByIdAsync(Id);
+            return Page();
+        }
+    }
+}
