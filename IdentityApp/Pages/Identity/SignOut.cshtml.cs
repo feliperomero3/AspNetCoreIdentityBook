fe73483fdb103ab83886aef796808ba45e7ede78
Felipe Romero <feliperomero3@outlook.com>
2021-08-16 23:38:07 -0500

    Signing Out of the Application.

---
+++ IdentityApp/Pages/Identity/SignOut.cshtml.cs
@@ -0,0 +1,24 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityApp.Pages.Identity
+{
+    public class SignOutModel : PageModel
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public SignOutModel(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public async Task<ActionResult> OnPostAsync()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToPage();
+        }
+    }
+}
