01ae5dc4433fd071ac339224fd829ddd58ac8d9c
Felipe Romero <feliperomero3@outlook.com>
2021-08-18 23:22:23 -0500

    Complete the User Dashboard.

--- IdentityApp/Pages/Identity/Index.cshtml.cs
+++ IdentityApp/Pages/Identity/Index.cshtml.cs
@@ -1,8 +1,26 @@
-﻿namespace IdentityApp.Pages.Identity
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace IdentityApp.Pages.Identity
 {
     public class IndexModel : UserPageModel
     {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public IndexModel(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public string Email { get; set; }
         public string Phone { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            var currentUser = await _userManager.GetUserAsync(User);
+
+            Email = currentUser.Email ?? "(No value)";
+            Phone = currentUser.PhoneNumber ?? "(No value)";
+        }
     }
 }
