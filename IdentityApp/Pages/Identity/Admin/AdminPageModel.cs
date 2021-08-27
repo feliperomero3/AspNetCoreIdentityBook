d2215fbccdcf7d10cc1d7d60527e1869fffde80d
Felipe Romero <feliperomero3@outlook.com>
2021-08-27 00:05:25 -0500

    Allow Access to the Identity Admin Razor Pages.

--- IdentityApp/Pages/Identity/Admin/AdminPageModel.cs
+++ IdentityApp/Pages/Identity/Admin/AdminPageModel.cs
@@ -1,8 +1,10 @@
 ﻿using System.Linq;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 
 namespace IdentityApp.Pages.Identity.Admin
 {
+    [AllowAnonymous]
     public class AdminPageModel : UserPageModel
     {
         public void ProcessIdentityOperationResult(IdentityResult result)
