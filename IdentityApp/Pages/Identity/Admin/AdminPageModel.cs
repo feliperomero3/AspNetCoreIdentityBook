836067b864c9d63142ab934075eeca33d5e4d80f
Felipe Romero <feliperomero3@outlook.com>
2021-09-28 01:10:32 -0500

    Apply the Authorization Policy.

--- IdentityApp/Pages/Identity/Admin/AdminPageModel.cs
+++ IdentityApp/Pages/Identity/Admin/AdminPageModel.cs
@@ -2,9 +2,9 @@
 
 namespace IdentityApp.Pages.Identity.Admin
 {
-    [AllowAnonymous]
+    [Authorize(Roles = "Dashboard")]
     public class AdminPageModel : UserPageModel
     {
-        // no methods or properties required
+        // No methods or properties required
     }
 }
