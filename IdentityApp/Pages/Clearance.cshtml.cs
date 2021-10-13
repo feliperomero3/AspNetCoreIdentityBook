b36bd82266373fc2af7672ffbff85ac77c4bc532
Felipe Romero <feliperomero3@outlook.com>
2021-10-12 21:15:35 -0500

    Using Claims Data.

---
+++ IdentityApp/Pages/Clearance.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IdentityApp.Pages
+{
+    public class ClearanceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
