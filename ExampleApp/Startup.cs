084e7a05c84cb71337ab1a54e06ed313deae0907
Felipe Romero <feliperomero3@outlook.com>
2021-11-09 23:33:02 -0600

    Configuring the Application.

--- ExampleApp/Startup.cs
+++ ExampleApp/Startup.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -12,19 +8,19 @@ namespace ExampleApp
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRazorPages();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -34,6 +30,8 @@ namespace ExampleApp
                 {
                     await context.Response.WriteAsync("Hello World!");
                 });
+
+                endpoints.MapRazorPages();
             });
         }
     }
