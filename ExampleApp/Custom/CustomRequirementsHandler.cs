45c87493d7ffbf93a5a02da908d53401b620009a
Felipe Romero <feliperomero3@outlook.com>
2021-11-17 22:51:05 -0600

    Defining the Custom Requirement and Handler.

---
+++ ExampleApp/Custom/CustomRequirementsHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ExampleApp.Custom
+{
+    public class CustomRequirementsHandler : IAuthorizationHandler
+    {
+        public Task HandleAsync(AuthorizationHandlerContext context)
+        {
+            foreach (var requirement in context.PendingRequirements.OfType<CustomRequirement>().ToList())
+            {
+                if (context.User.Identities.Any(identity => string.Equals(identity.Name, requirement.Name, StringComparison.OrdinalIgnoreCase)))
+                {
+                    context.Succeed(requirement);
+                }
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
