dcf322e14ac5cca5713251ac2408fe16a4721694
Felipe Romero <feliperomero3@outlook.com>
2021-11-22 21:48:38 -0600

    Creating the User Class.

---
+++ ExampleApp/Identity/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ExampleApp.Identity
+{
+    public class AppUser
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        public string UserName { get; set; }
+
+        public string NormalizedUserName { get; set; }
+    }
+}
