37324c1f4777ddfb482074a4d249c29bc84dafea
Felipe Romero <feliperomero3@outlook.com>
2021-11-23 21:55:49 -0600

    Creating the User Store - Implementing the Data Storage Methods.

---
+++ ExampleApp/Identity/Store/UserStoreCore.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace ExampleApp.Identity.Store
+{
+    public partial class UserStore : IUserStore<AppUser>
+    {
+        private readonly ConcurrentDictionary<string, AppUser> users = new ConcurrentDictionary<string, AppUser>();
+        private bool _disposed;
+
+        public Task<IdentityResult> CreateAsync(AppUser user, CancellationToken cancellationToken)
+        {
+            if (!users.ContainsKey(user.Id) && users.TryAdd(user.Id, user))
+            {
+                return Task.FromResult(IdentityResult.Success);
+            }
+            return Task.FromResult(Error);
+        }
+
+        public Task<IdentityResult> UpdateAsync(AppUser user, CancellationToken cancellationToken)
+        {
+            if (users.ContainsKey(user.Id))
+            {
+                users[user.Id].UpdateFrom(user);
+                return Task.FromResult(IdentityResult.Success);
+            }
+            return Task.FromResult(Error);
+        }
+
+        public Task<IdentityResult> DeleteAsync(AppUser user, CancellationToken cancellationToken)
+        {
+            if (users.ContainsKey(user.Id)
+            && users.TryRemove(user.Id, out user))
+            {
+                return Task.FromResult(IdentityResult.Success);
+            }
+            return Task.FromResult(Error);
+        }
+
+        public Task<AppUser> FindByIdAsync(string userId, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AppUser> FindByNameAsync(string normalizedUserName, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetNormalizedUserNameAsync(AppUser user, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetUserIdAsync(AppUser user, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> GetUserNameAsync(AppUser user, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetNormalizedUserNameAsync(AppUser user, string normalizedName, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetUserNameAsync(AppUser user, string userName, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static IdentityResult Error => IdentityResult.Failed(new IdentityError
+        {
+            Code = "StorageFailure",
+            Description = "User Store Error"
+        });
+
+        /// <summary>
+        /// Throws if this class has been disposed.
+        /// </summary>
+        protected void ThrowIfDisposed()
+        {
+            if (_disposed)
+            {
+                throw new ObjectDisposedException(GetType().Name);
+            }
+        }
+
+        /// <summary>
+        /// Dispose the store
+        /// </summary>
+        public void Dispose()
+        {
+            _disposed = true;
+            GC.SuppressFinalize(this);
+        }
+    }
+}
