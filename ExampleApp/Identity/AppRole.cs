684c7b463cf4050a4a682e135d027d7d09e3a1d3
Felipe Romero <feliperomero3@outlook.com>
2022-06-18 18:18:22 -0500

    Creating the Role class.

---
+++ ExampleApp/Identity/AppRole.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ExampleApp.Identity
+{
+    public class AppRole
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        public string Name { get; set; }
+
+        public string NormalizedName { get; set; }
+    }
+}
