7b13a20774ea8d627bef6f01df6ae3b26fb493c1
Felipe Romero <feliperomero3@outlook.com>
2022-06-25 14:48:51 -0500

    Signing In with Two Factors — Creating the Two-Factor Token Generator.

---
+++ ExampleApp/Identity/TwoFactorSignInTokenGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace ExampleApp.Identity
+{
+    public class TwoFactorSignInTokenGenerator : SimpleTokenGenerator
+    {
+        protected override int CodeLength => 3;
+
+        public override Task<bool> CanGenerateTwoFactorTokenAsync(UserManager<AppUser> manager, AppUser user)
+        {
+            return Task.FromResult(user.IsTwoFactorAuthenticationEnabled);
+        }
+    }
+}
