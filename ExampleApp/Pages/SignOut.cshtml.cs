88fbf8f842bfe81fdd7ee74c95bc93356fa2fc37
Felipe Romero <feliperomero3@outlook.com>
2022-06-25 16:08:19 -0500

    Signing In with Two Factors — Forgetting the Client.

--- ExampleApp/Pages/SignOut.cshtml.cs
+++ ExampleApp/Pages/SignOut.cshtml.cs
@@ -1,5 +1,7 @@
 using System.Threading.Tasks;
+using ExampleApp.Identity;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -7,6 +9,13 @@ namespace ExampleApp.Pages
 {
     public class SignOutModel : PageModel
     {
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public SignOutModel(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
         public string Username { get; private set; }
 
         public void OnGet()
@@ -14,8 +23,13 @@ namespace ExampleApp.Pages
             Username = User.Identity.Name ?? "(No Signed In User)";
         }
 
-        public async Task<ActionResult> OnPost()
+        public async Task<ActionResult> OnPost(string forgetMe)
         {
+            if (!string.IsNullOrEmpty(forgetMe))
+            {
+                await _signInManager.ForgetTwoFactorClientAsync();
+            }
+
             await HttpContext.SignOutAsync();
 
             return RedirectToPage("SignIn");
