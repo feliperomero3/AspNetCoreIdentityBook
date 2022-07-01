e13c52b7d22bce831803f2745346f69de4c42172
Felipe Romero <feliperomero3@outlook.com>
2022-06-30 20:27:55 -0500

    External Authentication — Preparing for External Authentication.

---
+++ ExampleApp/Custom/ExternalAuthenticationHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+
+namespace ExampleApp.Custom
+{
+    // An authentication handler that returns canned results, without actually performing authentication.
+    // This is a minimal implementation of the IAuthenticationHandler interface that contains just enough
+    // code to compile.
+    public class ExternalAuthenticationHandler : IAuthenticationHandler
+    {
+        private HttpContext _context;
+        private AuthenticationScheme _scheme;
+
+        public Task<AuthenticateResult> AuthenticateAsync()
+        {
+            return Task.FromResult(AuthenticateResult.NoResult());
+        }
+
+        public Task ChallengeAsync(AuthenticationProperties properties)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task ForbidAsync(AuthenticationProperties properties)
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task InitializeAsync(AuthenticationScheme scheme, HttpContext context)
+        {
+            _scheme = scheme;
+            _context = context;
+
+            return Task.CompletedTask;
+        }
+    }
+}
